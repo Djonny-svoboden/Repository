8ce992a80d6777805e1573d1b6b3c5a941fd6e2b
Djonni-svoboden <djinnic-forever15@gmail.com>
2019-11-29 02:30:51 +0200

    Proverka

--- home/main.js
+++ home/main.js
@@ -1,34 +1,35 @@
 var tab; // заголовок вкладки
-var tabCont; // блок содержащий контент вкладки
+var tabContent; // блок содержащий контент вкладки
 
 window.onload = function() {
-    tabCont = document.getElementsByClassName('tabs');
+    tabContent = document.getElementsByClassName('tabs');
     tab = document.getElementsByClassName('tab');
     hideTabs(1);
 }
 
 function hideTabs(a) {
-    for (var i = a; i < tabCont.length; i++) {
-        tabCont[i].classList.remove('show');
-        tabCont[i].classList.add("hide");
+    for (var i = a; i < tabContent.length; i++) {
+        tabContent[i].classList.remove('show');
+        tabContent[i].classList.add("hide");
         tab[i].classList.remove('ruibord');
     }
 }
-document.getElementById('tabs').onclick = function(ev) {
-    var target = ev.target;
-    if (target.className == "tab") {
-        for (var i = 0; i < tab.lenght; i++) {
+document.getElementById('tabs').onclick = function(event) {
+    var target = event.target;
+    if (target.className == 'tab') {
+        for (var i = 0; i < tab.length; i++) {
             if (target == tab[i]) {
-                showTabCont(i);
+                showTabs(i);
+                break;
             }
         }
     }
 }
 
-function showTabsContent(b) {
+function showTabs(b) {
     if (tabContent[b].classList.contains('hide')) {
-        hideTabsContent(0);
-        tab[b].classList.add('ruinbord');
+        hideTabs(0);
+        tab[b].classList.add('ruibord');
         tabContent[b].classList.remove('hide');
         tabContent[b].classList.add('show');
     }
